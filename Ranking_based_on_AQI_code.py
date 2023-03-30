1cf2d591bee0ddb1d589d9bb999bce43f1c8eeaa
Moorthy M Nair <83420459+moorthynair@users.noreply.github.com>
2023-03-30 13:39:06 +0530

    Update Ranking_based_on_AQI_code.py

--- Ranking_based_on_AQI_code.py
+++ Ranking_based_on_AQI_code.py
@@ -2,46 +2,63 @@
 """
 Created on Wed Nov  3 17:17:51 2021
 
-@author: HP
+@author: Moorthy M Nair
 """
 
 import pandas as pd
-import openaq
-import datetime
+import requests
 from tabula import read_pdf
 import numpy as np
-import os
+import sys
 
-##Input the file
-pathway = 'C:/Users/HP/Downloads/AQI_Bulletin_20211231.pdf'
+##User Defined Inputs
+date = input('Enter the date of analysis in the format YYYYMMDD: ') ##example 20230329
+pathway = input('Define the Pathway address to download the bulletin: ') ##example C:/Users/USER/Downloads
+list_of_cities = input('Enter the list of cities seperated by commas: ') ##Example Mumbai,Bangalore,Kolkata. Disclaimer: Make sure the cities mentioned are present in the Bulletin list
+
+############################# Analysis Begins ################################################
+## Step 1: Download the bulletin
+k = requests.get('https://cpcb.nic.in//upload/Downloads/AQI_Bulletin_'+str(date)+'.pdf')
+
+if k.status_code==404:
+    sys.exit('No Bulletin exists for the mentioned date. Plese try different date')
+ 
+with open(pathway+'AQI_Bulletin_'+str(date)+'.pdf','wb') as f:
+    f.write(k.content)
+    
+## Step 2: Read the downloaded bulletin file
+pathway = pathway+'AQI_Bulletin_'+str(date)+'.pdf'
 file = read_pdf(pathway,pages='all')
 
-
+## Step 3: Concat all the files to common dataframe
 new_file = pd.DataFrame()
-##Concat all the files to common dataframe
 for i in range (0, len(file)):
     if i%2==0:
         extract_file = file[i]
         new_file = pd.concat([new_file,extract_file], axis=0)
         
 
+## Step 4: Date Cleaning 
 ##Extract columns names
 k= new_file.loc[0, ]
 k.reset_index(inplace=True)
 column_names = k.loc[1, ]
 column_names = column_names[1: ]
 
-## Reset the columns
+## Reset the column names
 new_file.columns = column_names
 
-##Drop NA's from new_file
+## Drop NA's from the dataframe
 new_file.dropna(inplace=True)
 
+## Remove Duplicates and sorty by descending order of Index Value (Index here is AQI)
 final_file = new_file.loc[(new_file['City']!='City'), ]
 final_file['Index Value']= final_file['Index Value'].astype(int)
 final_file.sort_values(by='Index Value',ascending=False, inplace=True)
 final_file.reset_index(inplace=True)
 final_file.drop(columns=['S.No', 'index'], inplace=True)
+
+## Step 5: Assing the rank based on AQI values
 final_file['ranking'] = np.arange(1, len(final_file)+1)
 final_file['of Monitoring']= final_file['of Monitoring'].astype('int')
 
@@ -51,13 +68,13 @@ for i in range(0,len(final_file)-1):
         for k in range(i+2,len(final_file)):
             final_file.loc[k, 'ranking'] = final_file.loc[k, 'ranking']-1
 
-city_list = ['Bettiah','Bhagalpur','Bihar Sharif','Buxar','Chhapra','Darbhanga','Gaya','Hajipur',
-             'Katihar','Kishanganj','Manguraha','Motihari','Munger','Muzaffarpur','Patna','Purnia',
-             'Rajgir','Saharsa','Sasaram','Siwan']        
+final_file ## Final result of analysis for all the cities mentioned in the Bulletin
+            
+## Filter the city specific rank
+city_list = list_of_cities.split(',')     
 city_ranking = final_file.loc[final_file['City'].isin(city_list), ]
 
-
-
-
-
-
+if len(city_ranking)==0:
+    sys.exist('No information for the list of cities as inputed are available in the AQI Bulletin')
+    
+city_ranking ## Final result for list of cities as per the user input
